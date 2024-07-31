f1712f76646affd7ed833dab09d66dc43fb9a444
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-08-01 02:15:49 +0800

    Add rbenv

--- .zsh_config/mac/rc.zsh
+++ .zsh_config/mac/rc.zsh
@@ -1,6 +1,7 @@
 source ~/.zsh_config/mac/aliases.zsh
 source ~/.zsh_config/mac/brew.zsh
 source ~/.zsh_config/mac/node.zsh
+source ~/.zsh_config/mac/ruby.zsh
 source ~/.zsh_config/mac/oh-my-zsh.zsh
 
 [ -z "$TMUX"  ] && { tmux a || tmux;}
