e4b28608ff3bd52ffd7b1cb14c82945cae750ad0
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-08-20 15:36:47 +0800

    Add NVM_NODEJS_ORG_MIRROR for nvm

--- .zsh_config/mac/node.zsh
+++ .zsh_config/mac/node.zsh
@@ -2,6 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+export NVM_NODEJS_ORG_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
 
 # pnpm
 export PNPM_HOME="/Users/andy/Library/pnpm"
