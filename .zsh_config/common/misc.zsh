a359e274d80b776ad819d7270676a02b42e724ee
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-08-27 20:28:29 +0800

    Add Dart path

--- .zsh_config/common/misc.zsh
+++ .zsh_config/common/misc.zsh
@@ -9,4 +9,5 @@ export LESS=" -R -X "
 [[ -f ~/.LESS_TERMCAP ]] && source ~/.LESS_TERMCAP
 
 export PATH="/opt/homebrew/anaconda3/bin":$PATH
+export PATH="/opt/homebrew/opt/dart@2.19/bin:$PATH"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH
