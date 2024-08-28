992d706bdf77a63c0abc4bb1d823cb6155e67a8d
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-08-28 13:52:26 +0800

    Move brew installed apps path

--- .zsh_config/common/misc.zsh
+++ .zsh_config/common/misc.zsh
@@ -7,7 +7,3 @@ export TERM=xterm-256color
 
 export LESS=" -R -X "
 [[ -f ~/.LESS_TERMCAP ]] && source ~/.LESS_TERMCAP
-
-export PATH="/opt/homebrew/anaconda3/bin":$PATH
-export PATH="/opt/homebrew/opt/dart@2.19/bin:$PATH"
-export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
