9eeda1f6ac6af0d162480a2a1a3f9a2805258be8
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-09-14 22:17:39 +0800

    Add OCaml / opam

---
+++ .zsh_config/mac/ocaml.zsh
@@ -0,0 +1,7 @@
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+[[ ! -r '/Users/andy/.opam/opam-init/init.zsh' ]] || source '/Users/andy/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+# END opam configuration
--- .zsh_config/mac/rc.zsh
+++ .zsh_config/mac/rc.zsh
@@ -2,6 +2,7 @@ source ~/.zsh_config/mac/aliases.zsh
 source ~/.zsh_config/mac/brew.zsh
 source ~/.zsh_config/mac/node.zsh
 source ~/.zsh_config/mac/ruby.zsh
+source ~/.zsh_config/mac/ocaml.zsh
 source ~/.zsh_config/mac/oh-my-zsh.zsh
 
 [ -z "$TMUX"  ] && { tmux a || tmux;}
