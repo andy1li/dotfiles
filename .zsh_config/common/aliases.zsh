1fa9cebc77b5bb08d5019adfa36fd51995bafb3e
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-09-20 07:14:25 +0800

    Add git shortcuts

--- .zsh_config/common/aliases.zsh
+++ .zsh_config/common/aliases.zsh
@@ -1,6 +1,12 @@
 alias g="git"
 alias gr="grep"
 
+alias gco="git checkout"
+alias gcob="git checkout -b"
+alias gcom="git checkout main"
+alias glg="git log --oneline --decorate --all --graph"
+alias gs="git status -s"
+
 alias rl="readlink"
 
 alias t="tmux"
