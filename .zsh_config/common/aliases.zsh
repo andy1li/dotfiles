ce2ee1f2003bfb32d6ee85f737b118198f8b44cf
Andy Li <1450947+andy1li@users.noreply.github.com>
2024-11-22 11:11:12 +0800

    Add gcan and gf

--- .zsh_config/common/aliases.zsh
+++ .zsh_config/common/aliases.zsh
@@ -1,9 +1,11 @@
 alias g="git"
 alias gr="grep"
 
+alias gcan="git commit --amend --no-edit"
 alias gco="git checkout"
 alias gcob="git checkout -b"
 alias gcom="git checkout main"
+alias gf="git push --force-with-lease"
 alias gl="git pull"
 alias glg="git log --oneline --decorate --all --graph"
 alias gs="git status -s"
