6dcc742dadca89b45db8e7d367b565104488c3d9
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-06-10 15:07:59 +0800

    feat(zsh): add TMUX window renaming functionality for improved session management

--- .zsh_config/common/misc.zsh
+++ .zsh_config/common/misc.zsh
@@ -7,3 +7,15 @@ export TERM=xterm-256color
 
 export LESS=" -R -X "
 [[ -f ~/.LESS_TERMCAP ]] && source ~/.LESS_TERMCAP
+
+preexec() {
+  [[ -n "$TMUX" ]] || return
+  [[ -z "$TMUX_OLD_WINDOW_NAME" ]] && TMUX_OLD_WINDOW_NAME=$(tmux display-message -p '#W')
+  tmux rename-window "${1%% *}"
+}
+
+precmd() {
+  [[ -n "$TMUX" && -n "$TMUX_OLD_WINDOW_NAME" ]] || return
+  tmux rename-window "$TMUX_OLD_WINDOW_NAME"
+  unset TMUX_OLD_WINDOW_NAME
+}
