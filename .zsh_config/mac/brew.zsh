743554d78c624a56ad0007205fb45ed5505efcf9
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-06-21 22:43:36 +0800

    chore(brew.zsh): reorder VCPKG_ROOT export for consistency and clarity

--- .zsh_config/mac/brew.zsh
+++ .zsh_config/mac/brew.zsh
@@ -6,10 +6,8 @@ export PATH="/opt/homebrew/opt/go@1.23/bin:$PATH"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH="/opt/homebrew/opt/rustup/bin:$PATH"
 
-export VCPKG_ROOT="$HOME/vcpkg"
-
 export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
 export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
-export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
-export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
 export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
+
+export VCPKG_ROOT="$HOME/vcpkg"
