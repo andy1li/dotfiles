9be4e37e06599468720289caa7165ae97eb840bc
Andy Li <1450947+andy1li@users.noreply.github.com>
2026-01-30 13:48:03 +0900

    feat(zsh): comment out NVM mirror and add bun completions; update iTerm2 plist with new AI model and token settings

--- .zsh_config/mac/node.zsh
+++ .zsh_config/mac/node.zsh
@@ -2,7 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-export NVM_NODEJS_ORG_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
+# export NVM_NODEJS_ORG_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/
 
 # pnpm
 export PNPM_HOME="/Users/andy/Library/pnpm"
@@ -11,3 +11,6 @@ case ":$PATH:" in
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 # pnpm end
+
+# bun completions
+[ -s "/Users/andy/.bun/_bun" ] && source "/Users/andy/.bun/_bun"
