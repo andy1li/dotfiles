23f981ea55b6f3d694eb7ad52abec0310601ebe2
Andy Li <1450947+andy1li@users.noreply.github.com>
2026-05-02 22:35:44 +0900

    chore(brew.zsh): comment out Homebrew configuration for clarity and consistency

--- .zsh_config/mac/brew.zsh
+++ .zsh_config/mac/brew.zsh
@@ -6,8 +6,8 @@ export PATH="/opt/homebrew/opt/go@1.23/bin:$PATH"
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH="/opt/homebrew/opt/rustup/bin:$PATH"
 
-export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
-export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
-export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
+# export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+# export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+# export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
 
 export VCPKG_ROOT="$HOME/vcpkg"
